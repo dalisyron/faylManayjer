ad901bd543f628ae0fa92d3a777f39bd87fbf8ae
hamid_s_k <shrsanaee@gmail.com>
2018-04-10 13:23:15 +0430

    adding a json converter func in item

--- item.py
+++ item.py
@@ -1,7 +1,7 @@
 import time
 import os
 import shutil
-#github usernames added
+
 
 class Item:
     def __init__(self, path, name, date, size, par_item=None):
@@ -11,16 +11,11 @@ class Item:
         self.size = size
         self.par_item = par_item
 
-    def copy(self, destination, cut_flag):
-        new_name = self.name
+    def copy(self, destination):
         if os.path.isdir(self.path + '/' + self.name):
-            while (os.path.exists(destination + '/' + new_name)):
-                new_name = rename_copy(new_name)
-            shutil.copytree(self.path + '/' + self.name, destination + '/' + new_name)
+            shutil.copytree(self.path + '/' + self.name, destination + '/' + self.name)
         else:
-            while (os.path.exists(destination + '/' + new_name)):
-                new_name = rename_copy(new_name)
-            shutil.copyfile(self.path + '/' + self.name, destination + '/' + new_name)
+            shutil.copyfile(self.path + '/' + self.name, destination + '/' + self.name)
 
     def delete(self):
         if os.path.isdir(self.path + '/' + self.name):
@@ -33,14 +28,15 @@ def getItemList(path):
     str_file_list = os.listdir(path)
     result = []
     for i in str_file_list:
-        #tmp_item = Item(path, i, time.ctime(os.path.getmtime(path + '/' + i)), os.path.getsize(path + '/' + i))
-        slash = '/'
-        if (path == '/'):
-            slash = ''
-        tmp_item = Item(path, i, time.ctime(os.path.getmtime(path + slash + i)), os.path.getsize(path + slash + i))
+        tmp_item = Item(path, i, time.ctime(os.path.getmtime(path + '/' + i)), os.path.getsize(path + '/' + i))
         result.append(tmp_item)
     return result
+def getItemJson(path):
+    dict = {}
+    itemList = getItemList(path)
+    print(type(itemList[0].date))#str
+    print(type(itemList[0].size))#int
+    for i in range(len(itemList)):
+        dict[i] = itemList[i].path + ',' + itemList[i].name + ',' + str(itemList[i].date) + ',' + str(itemList[i].size)
 
-def rename_copy(dir_name):
-    desc_list = dir_name.split('.')
-    return desc_list[0] + ' (Copy).' + ''.join(desc_list[1:])
+    return dict
